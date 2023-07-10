85d4220a9e108dcc86b21876d9240a96cad09049
phuphaGod <105551327+phuphaGod@users.noreply.github.com>
2023-07-10 10:40:55 +0700

    Add files via upload

---
+++ Fistproject 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{ cout << "Student Code : 6606021621143"<< endl;
+  cout << "Student Name : Thanapoom raksin"<< endl;
+  cout << "   Division  : Information Technology"<< endl;
+  cout << "        Year : 1"<< endl;
+  cout << "        Room : RB"<< endl;
+  cout << "        sex  : Male"<< endl;
+  cout << "        Age  : 18";
+  system("pause");
+  return (0);
+}	
